c9a4c302ca343308570845c9a536b8aac6c03a5d
Ashish Dongare <ashudongare000@gmail.com>
2024-06-25 22:12:18 +0530

    add exception handling

--- src/app/restaurant/restaurant.service.ts
+++ src/app/restaurant/restaurant.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +11,37 @@ export class RestaurantService {
   constructor(private http: HttpClient) { }
 
   getRestaurants(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrl).pipe(catchError(this.handleError));
   }
 
   addRestaurant(restaurant: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, restaurant);
+    return this.http.post<any>(this.apiUrl, restaurant).pipe(catchError(this.handleError));
   }
 
   getRestaurantById(id: number): Observable<any> {
     const url = `${this.apiUrl}/${id}`;
-    return this.http.get<any>(url);
+    return this.http.get<any>(url).pipe(catchError(this.handleError));
   }
 
   updateRestaurant(restaurant: any): Observable<any> {
     const url = `${this.apiUrl}/${restaurant.id}`;
-    return this.http.put<any>(url, restaurant);
+    return this.http.put<any>(url, restaurant).pipe(catchError(this.handleError));
   }
 
   deleteRestaurant(id: number): Observable<any> {
     const url = `${this.apiUrl}/${id}`;
-    return this.http.delete<any>(url);
+    return this.http.delete<any>(url).pipe(catchError(this.handleError));
+  }
+
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error occurred';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
